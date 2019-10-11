75a42e3ec676abb37424324980a626425533cafa
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-11 10:22:34 +0300

    Tables folder added

--- SQLServerExample/SQLServerExample/Phone.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [dbo].[Phone]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
---
+++ SQLServerExample/SQLServerExample/Tables/Phone.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Phone]
+(
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Type] [varchar](max) NULL,
+	[Number] [nvarchar](50) NULL,
+	[PersonId] [bigint] NULL, 
+    CONSTRAINT [PK_Phone] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_Phone] FOREIGN KEY ([PersonId]) REFERENCES [Person]([Id]),
+)
