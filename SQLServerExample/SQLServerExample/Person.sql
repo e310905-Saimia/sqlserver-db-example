075308d4a6c858b9620aaf1d8c7df51e3f6fcc9d
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-11 10:20:37 +0300

    Tables added

---
+++ SQLServerExample/SQLServerExample/Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person]
+(	
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NULL,
+	[Age] [smallint] NULL,
+	[Psw] [varchar](max) NULL, 
+    CONSTRAINT [PK_Person] PRIMARY KEY ([Id]),
+)
---
+++ SQLServerExample/SQLServerExample/Phone.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Phone]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
